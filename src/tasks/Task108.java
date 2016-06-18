a5f35f4a1248ea47285e12b2ec403b14fc8bdae4
Aska <aska.fed@gmail.com>
2016-06-18 16:38:07 +0300

    add helper

--- src/tasks/Task108.java
+++ src/tasks/Task108.java
@@ -4,28 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/** The {@code tasks.Task108} class represents calculate of the task108.
+/**
+ * The {@code tasks.Task108} class represents calculate of the task108.
+ *
  * @author aska
- * Created on 15.06.2016.
+ *         Created on 15.06.2016.
  */
 
 public class Task108 implements Task {
 
     @Override
     public void solution() {
-        System.out.println("Умова задачі №108: ");
-        System.out.println("Дано натуральное число n. " +
-                "Получить наименьшее число вида 2^r," +
-                "превосходящее n. \n");
-        System.out.println("Введіть натуральне число n:");
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = 0;
-        try {
-            n = Integer.parseInt(bufferedReader.readLine());
-        } catch (IOException e) {
-            System.err.println("Не вдається отримати вхідні дані.");
-        }
+        Helper helper = new Helper();
+        helper.printTask("108", "Дано натуральное число n. \" +\n" +
+                "                \"Получить наименьшее число вида 2^r,\" +\n" +
+                "                \"превосходящее n.");
+        int n = helper.getIntInput();
 
         int r = 0;
         try {
@@ -34,12 +28,13 @@ public class Task108 implements Task {
             System.out.println(e.getMessage());
             return;
         }
-        System.out.println("Відповідь: 2^" + r + "=" + (int) Math.pow(2, r));
-
+        helper.printSolution("2^" + r + "=" + (int) Math.pow(2, r));
     }
 
-    /** This private method finds perform calculation.
+    /**
+     * This private method finds perform calculation.
      * Finds min number represented as 2^r that is greater than n.
+     *
      * @param n incoming positive number
      * @return {@code int} number represented as 2^r that is greater than n
      * @throws IllegalArgumentException if param n isn't positive
