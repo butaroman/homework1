b601affc0970c27b82963a2e40c0d5c175c2b002
NazDov <nazardovhuy@gmail.com>
2016-06-20 01:17:37 +0300

    added task 560

---
+++ src/tasks/Task560.java
@@ -0,0 +1,17 @@
+package tasks;
+
+/**
+ * Created by nazar.dovhyy on 20.06.2016.
+ */
+public class Task560 implements Task {
+
+
+
+    
+
+
+    @Override
+    public void solution() {
+
+    }
+}
