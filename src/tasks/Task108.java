65871f1ca8ad8710dea7888484caa7dfcdf146ff
Cavayman <cavayman@gmail.com>
2016-06-21 17:18:52 +0300

    maked usable method public

--- src/tasks/Task108.java
+++ src/tasks/Task108.java
@@ -47,7 +47,7 @@ public class Task108 extends AbstractTask {
 	 * @throws IllegalArgumentException
 	 *             if param n isn't positive
 	 */
-	private int calc(int n) {
+	public int calc(int n) {
 		if (n < 0) {
 			throw new IllegalArgumentException("Invalid parameter, n must be positive.");
 		}
