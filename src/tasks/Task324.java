ce64ae1474be17928b262809af9d927993e427c1
NazDov <nazardovhuy@gmail.com>
2016-06-21 19:17:15 +0300

    introduced inner class for testing purposes

--- src/tasks/Task324.java
+++ src/tasks/Task324.java
@@ -1,111 +1,132 @@
 package tasks;
 
 
+import home.utils.AbstractTask;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import home.utils.AbstractTask;
 
 /**
  * Created by nazar.dovhyy on 20.06.2016.
  */
 public class Task324 extends AbstractTask {
 
-	private int[] arr;
 
-	@Override
-	public String solution() {
-		int n = 0;
-		try {
+    @Override
+    public String solution() {
+        int n = 0;
+        try {
 
-			n = readPositiveNumber();
-		} catch (IOException e) {
-			return "";
-		}
+            n = readPositiveNumber();
+        } catch (IOException e) {
+            return "";
+        }
 
-		String output = null;
-		List<Integer> allPrimeNumbers = findAllPrimeNumbersFor(n);
-		StringBuilder builder = new StringBuilder();
+        String output = null;
+        List<Integer> allPrimeNumbers = findAllPrimeNumbersFor(n);
+        StringBuilder builder = new StringBuilder();
 
-		for (int i = 0; i < allPrimeNumbers.size(); i++) {
+        for (int i = 0; i < allPrimeNumbers.size(); i++) {
 
-			output = getFormattedOutput(i, allPrimeNumbers, builder).toString();
-		}
+            output = getFormattedOutput(i, allPrimeNumbers, builder).toString();
+        }
 
-		return output;
-	}
+        return output;
+    }
 
-	@Override
-	public String getTaskNumber() {
-		return "324";
-	}
+    @Override
+    public String getTaskNumber() {
+        return "324";
+    }
 
-	@Override
-	public String getTaskCondition() {
-		return "Given a natural number n. Find all numbers less than n being mutually prime with n.";
-	}
+    @Override
+    public String getTaskCondition() {
+        return "Given a natural number n. Find all numbers less than n being mutually prime with n.";
+    }
 
-	private StringBuilder getFormattedOutput(int index, List<Integer> allPrimeNumbers, StringBuilder builder) {
+    private StringBuilder getFormattedOutput(int index, List<Integer> allPrimeNumbers, StringBuilder builder) {
 
-		return (index < allPrimeNumbers.size() - 1) ? builder.append(allPrimeNumbers.get(index)).append(", ")
-				: builder.append(allPrimeNumbers.get(index)).append(".");
-	}
+        return (index < allPrimeNumbers.size() - 1) ? builder.append(allPrimeNumbers.get(index)).append(", ")
+                : builder.append(allPrimeNumbers.get(index)).append(".");
+    }
 
-	private List<Integer> findAllPrimeNumbersFor(int number) {
+    private List<Integer> findAllPrimeNumbersFor(int number) {
 
-		fillArrayHolder(number);
+        InnerNumberHolder.fillArrayHolder(number);
 
-		return primeNumbersForN(number);
-	}
+        return primeNumbersForN(number);
+    }
 
-	private List<Integer> primeNumbersForN(int number) {
+    private List<Integer> primeNumbersForN(int number) {
 
-		List<Integer> primeNumbers = new ArrayList<>();
+        List<Integer>  primeNumbers = InnerNumberHolder.returnAllPrimeNumbersFor(number);
 
-		for (int i = number; i < number + 1; i++) {
+        return primeNumbers;
+    }
 
-			for (int j = 0; j < arr.length; j++) {
 
-				if (retPrimeNumber(i, arr[j]) == 1) {
+    /**
+     * added for testing purposes
+     */
+    public static class InnerNumberHolder {
 
-					primeNumbers.add(arr[j]);
-				}
+        /**
+         * array holder.
+         */
+        private static int[] arr;
 
-			}
-		}
+        private static List<Integer> returnAllPrimeNumbersFor(int number) {
 
-		return primeNumbers;
-	}
+            List<Integer> innerPrimeNumberList = new ArrayList<>();
 
-	private int retPrimeNumber(int firstNum, int secNum) {
+            for (int i = number; i < number + 1; i++) {
 
-		if (firstNum % secNum == 0) {
-			return secNum;
-		}
+                for (int j = 0; j < arr.length; j++) {
 
-		return retPrimeNumber(secNum, firstNum % secNum);
-	}
+                    if (retPrimeNumber(i, arr[j]) == 1) {
 
-	private void fillArrayHolder(int number) {
+                        innerPrimeNumberList.add(arr[j]);
+                    }
 
-		if (number < 1) {
-			throw new IllegalArgumentException("the input number must be larger than 0");
-		}
+                }
+            }
 
-		arr = new int[number - 1];
+            return innerPrimeNumberList;
+        }
 
-		fillArrayHolderWithNumbers(number - 1);
 
-	}
+        public static int retPrimeNumber(int firstNum, int secNum) {
 
-	private void fillArrayHolderWithNumbers(int number) {
+            if (firstNum % secNum == 0) {
+                return secNum;
+            }
 
-		int count = 0;
+            return retPrimeNumber(secNum, firstNum % secNum);
+        }
 
-		while (count < number) {
-			arr[count++] = count;
-		}
+        public static int[] fillArrayHolder(int number) {
 
-	}
+            if (number < 1) {
+                throw new IllegalArgumentException("the input number must be larger than 0");
+            }
+
+            return arr == null ? fillArrayHolderWithNumbers(number - 1) : Arrays.copyOf(arr, arr.length);
+
+        }
+
+        private static int[] fillArrayHolderWithNumbers(int number) {
+
+            arr = new int[number];
+
+            int count = 0;
+
+            while (count < arr.length) {
+                arr[count++] = count;
+            }
+
+            return arr;
+        }
+    }
 }
