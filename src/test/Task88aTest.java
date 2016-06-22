96a824713b042fb655307487ceeeb04b8311412f
vilumkris <vilumkris@gmail.com>
2016-06-22 15:48:42 +0300

    first commit

---
+++ src/test/Task88aTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+import tasks.Task88a;
+
+/**
+ * Created by Администратор on 21.06.2016.
+ */
+public class Task88aTest {
+
+    @Test
+    public void testTask88a(){
+        Task88a task88a = new Task88a();
+        task88a.setNumber(8);
+
+    }
+}
